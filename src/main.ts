1bb6b2df49f4b0dee095f666a780d905d1ef6492
NDJafu <giaphu432@gmail.com>
2025-04-09 00:23:42 +0700

    update: remove fastify adapter

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import {
-  FastifyAdapter,
-  type NestFastifyApplication,
-} from '@nestjs/platform-fastify';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { HttpExceptionFilter } from './utils/http-exceptions.filter';
 
 const PORT = 4003;
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     AppModule,
-    new FastifyAdapter(),
-  );
-
-  app.connectMicroservice<MicroserviceOptions>({
-    transport: Transport.TCP,
-    options: {
-      host: '127.0.0.1',
-      port: PORT,
+    {
+      transport: Transport.TCP,
+      options: {
+        host: '127.0.0.1',
+        port: PORT,
+      },
     },
-  });
+  );
   app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen();
-  console.log(`✅ Bus Management Service is listening on port ${PORT}`);
+  console.log(`✅ Payment Service is listening on port ${PORT}`);
 }
 bootstrap();
